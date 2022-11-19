a47c0fd9454e9b3c74cebebe41c47bed5792970e
Jimmy <jimmyzhang1@hotmail.com>
2022-11-18 19:08:35 -0800

    sum a to b recursive function

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
+// Our base case, when i = toN
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let total = 0;
+
+  while (fromN <= toN) {
+    return total += fromN + sum(fromN + 1, toN);
+  }
+
+  return total;
 }
 
 module.exports = sum;
+
+// 3 + sum(4, toN);
+// 3 + 4 + sum(5, toN) + ... + sum(toN, toN);
